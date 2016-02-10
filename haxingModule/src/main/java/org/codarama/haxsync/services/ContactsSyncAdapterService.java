a317e4555531c26348d1c4b5f14d971784761195
tishun <tihomir.mateev@gmail.com>
2016-02-10 10:03:18 +0200

    Update copyright
    As per http://choosealicense.com/licenses/ and http://www.gnu.org/licenses/license-list.html we are "stuck" with GNU 2, as the original source code of the fork is licensed under it.
    
    Will do a bit of more research if we can upgrade to GNU 2, but will leave this copyright for now.

--- haxingModule/src/main/java/org/codarama/haxsync/services/ContactsSyncAdapterService.java
+++ haxingModule/src/main/java/org/codarama/haxsync/services/ContactsSyncAdapterService.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2016 Codarama.org, All Rights Reserved
+ *
+ * Codarama HaxSync is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * Codarama HaxSync is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package org.codarama.haxsync.services;
 
 import android.accounts.Account;
@@ -62,7 +80,7 @@ import java.util.regex.Pattern;
  */
 @Deprecated
 public class ContactsSyncAdapterService extends Service {
-    private static final String TAG = "ContactsSyncAdapterService";
+    private static final String TAG = "LegacyAdapterService";
     public static ContentResolver mContentResolver = null;
     private static SyncAdapterImpl sSyncAdapter = null;
     private static String UsernameColumn = ContactsContract.RawContacts.SYNC1;
